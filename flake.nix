b908514e07493fffba3c1cd9e8a564aa6e98d0bb
DaveVED <dave.w.dennis@gmail.com>
2025-06-08 22:59:18 -0500

    feat: first flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "🟪";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs_22
+            git
+            bun
+          ];
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ];
+          shellHook = ''
+            echo "🟪 bingbong 🟪"
+            echo "Tauri development environment loaded!"
+          '';
+        };
+      });
+}
